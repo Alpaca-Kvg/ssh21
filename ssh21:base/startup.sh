5f757e930e5789dc74b390e867c67bcf3ebd3b45
isx23932486 <isx23932486@i09.informatica.escoladeltreball.org>
2021-11-16 13:43:14 +0100

    ssh:base

--- ssh21:base/startup.sh
+++ ssh21:base/startup.sh
@@ -7,13 +7,20 @@ echo -e "unix01\nunix01" | passwd unix01
 echo -e "unix02\nunix02" | passwd unix02
 echo -e "unix03\nunix03" | passwd unix03
 
+cp /opt/docker/nslcd.conf /etc/
 cp /opt/docker/login.defs /etc/login.defs
-
-/usr/sbin/sshd
-service start nscd
-/usr/sbin/nslcd  
-
-
+cp /opt/docker/pam_mount.conf.xml /etc/security
+cp /opt/docker/nsswitch.conf /etc
+cp /opt/docker/common-session /etc/pam.d/
+chmod 600 id_rsa.pub
+mkdir /home/unix01/.ssh
+cp id_rsa.pub /home/unix01/.ssh/authorized_keys
+chown unix01.unix01 /home/unix01/.ssh/authorized_keys
+/etc/init.d/ssh start
+/etc/init.d/ssh stop
+service nscd start
+service nslcd start 
+/usr/sbin/sshd -d
 
 
 
